4883664c7f3ae22e64c7ac80b1689a3455ca5fa8
mook <763944797@qq.com>
2018-12-14 23:37:58 +0800

    项目初始化

---
+++ src/main/java/com/cdream/testpaper/TestpaperApplication.java
@@ -0,0 +1,14 @@
+package com.cdream.testpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpaperApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/cdream/testpaper/TestpaperApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cdream.testpaper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestpaperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
